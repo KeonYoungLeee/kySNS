495886866ea32d1012e7697187c247789dc25516
KeonYoungLeee <ygy5036@gmail.com>
2020-09-20 23:35:29 +0900

    process env Setting

--- client/src/firebaseinstance.js
+++ client/src/firebaseinstance.js
@@ -4,7 +4,7 @@ import '@firebase/auth'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyDsARemg0GhOFhMp9yd5zoGvqsEkyzXqQ4',
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
